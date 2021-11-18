930837efc007fd8a8718edafdb515c4d3938277e
declan <manh.n@trustana.com>
2021-11-18 10:34:50 +0700

    update serverless config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import app from './app';
+import serverless from "serverless-http";
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
-})
+});
+
+module.exports.handler = serverless(app);
