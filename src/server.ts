234cc2b6f5ecf1a8e4445be674a452143c7bb906
declan <manh.n@trustana.com>
2021-11-22 09:57:06 +0700

    env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
-import app from './app';
+import dotenv from 'dotenv';
 import serverless from "serverless-http";
+import mongoose from 'mongoose';
+import app from './app';
 
-const PORT = 3000;
+const result = dotenv.config()
+
+if (result.error) {
+    throw result.error
+}
+
+const PORT: number = Number(process.env.PORT);
+mongoose.connect(`${ process.env.MONGO_URL }`, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Mongo connect successfully!');
+    }
+});
+
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 });
